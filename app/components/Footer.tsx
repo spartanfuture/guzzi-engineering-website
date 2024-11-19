6abb64f3fca68b3c4563e581b3f094e8afa89b37
sam-jay-dev <126140330+sam-jay-dev@users.noreply.github.com>
2024-11-19 16:12:06 +0800

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,13 +1,8 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookF, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
-import localFont from 'next/font/local'
-import logoSrc from './logo'
-
-const ethnocentric = localFont({ src: '../fonts/ethnocentric-rg.woff' })
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import logoSrc from "./logo";
+import { SocialLinks } from "./SocialLinks";
 
 export default function Footer() {
   return (
@@ -15,7 +10,7 @@ export default function Footer() {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center mb-8">
           {/* Logo and Company Name */}
-          <div className="flex items-center mb-4 md:mb-0 w-full md:w-1/4">
+          <div className="flex items-center mb-4 md:mb-0 w-full sm:w-1/4 justify-center md:justify-normal">
             <Image
               src={logoSrc}
               alt="Spartan Future Logo"
@@ -23,19 +18,18 @@ export default function Footer() {
               height={50}
               className="w-12 h-12"
             />
-           
           </div>
 
           {/* Navigation */}
           <nav className="mb-4 md:mb-0 w-full md:w-1/2">
             <ul className="flex flex-wrap justify-center space-x-6">
               {[
-                { name: 'Electric Future', href: '/#electric-future' },
-                { name: 'Services', href: '/#services' },
-                { name: 'Contact', href: '/#contact' }
+                { name: "Electric Future", href: "/#electric-future" },
+                { name: "Services", href: "/#services" },
+                { name: "Contact", href: "/#contact" },
               ].map((item) => (
                 <li key={item.name}>
-                  <Link 
+                  <Link
                     href={item.href}
                     className="hover:text-blue-400 transition duration-300 text-sm font-semibold"
                   >
@@ -46,30 +40,19 @@ export default function Footer() {
             </ul>
           </nav>
 
-          {/* Social Icons */}
-          <div className="flex space-x-4 w-full md:w-1/4 justify-center md:justify-end">
-            {[
-               { icon: faFacebookF, href: "https://www.facebook.com/spartanfutureofficial" },
-               { icon: faInstagram, href: "https://www.instagram.com/spartanfutureofficial" },
-               { icon: faEnvelope, href: "mailto:support@spartanfuture.com" },
-               { icon: faPhone, href: "tel:+1234567890" }
-            ].map((item, index) => (
-              <a 
-                key={index} 
-                href={item.href} 
-                className="text-white hover:text-blue-400 transition duration-300 transform hover:scale-110"
-              >
-                <FontAwesomeIcon icon={item.icon} size="sm" />
-              </a>
-            ))}
+          <div className="w-100 md:w-1/4 justify-items-end">
+            <SocialLinks mobile />
           </div>
         </div>
 
         {/* Copyright */}
         <div className="text-center text-sm text-gray-400">
-          <p>&copy; {new Date().getFullYear()} Spartan Future. All rights reserved.</p>
+          <p>
+            &copy; {new Date().getFullYear()} Spartan Future. All rights
+            reserved.
+          </p>
         </div>
       </div>
     </footer>
-  )
+  );
 }
